16ab5ccf5493a78d847b6328d0a58fc4bf7aea93
Sreeja Bavandla <144440011+sreejabavandla@users.noreply.github.com>
2025-07-29 15:40:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://sreeja-portfolio-eta.vercel.app', // your Vercel frontend
+  methods: ['GET', 'POST'],
+}));
 app.use(express.json());
 
 // MongoDB connection
